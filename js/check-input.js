ef90b68c4a2718d641d1b48cb43a2885237e184a
RaHsu <rahsuchina@163.com>
2017-09-26 13:13:49 +0800

    新增 第一次上传

---
+++ js/check-input.js
@@ -0,0 +1,57 @@
+/**
+ * Created by rahsu on 2017/9/21.
+ */
+//验证方法集
+function isEmpty(obj) {
+    if(obj ==='' || obj === null || obj === []){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function isNumber(obj){
+    var pattern = /^[0-9]*$/;
+    if(pattern.test(obj)===false){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function checkInput(data){
+    var i = 0,j = 0;
+    var flag = true;
+
+    //首先遍历每个表单元素
+    for(i in data){
+        //再判断要使用哪些方法来验证表单元素
+        for(j = 0;j<data[i].check.length;j++){
+
+            //判断一个表单数据是否为空
+            if(data[i].check[j] === 'empty'){
+                if(isEmpty(data[i].value)===false){
+                    flag = false;
+                    this.$Message.error(data[i].alertText.empty);
+                    return false;
+                }
+            }
+
+            // 判断一个表单数据是否为数字
+            if(data[i].check[j] === 'number'){
+                if(isNumber(data[i].value)===false){
+                    flag = false;
+                    this.$Message.error(data[i].alertText.number);
+                    return false;
+                }
+            }
+        }
+
+
+    }
+    if(flag === true){
+        return true;
+    }
+}
