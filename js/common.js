369f554f6d36d6024f14f8f8fc0104b6df1b874b
RaHsu <rahsuchina@163.com>
2017-09-28 09:53:05 +0800

    修改

--- js/common.js
+++ js/common.js
@@ -24,15 +24,17 @@ function get_cookie(){
 
 // 查询某单个权限是否存在
 function check_auth() {
-    var send_cookie= get_cookie();
-    var send_username = send_cookie.username;
     $.ajax({
         //url : '/CTT-MS-server/checkauth',
         url : 'http://10.2.130.178/CTT-MS-server/checkauth',
         type : 'POST',
+        //dataType:"json",
         success:function (result) {
-            console.log(result);
-
+            console.log(result.state);
+            if(result.state === 'error'){
+                alert(result.message);
+                window.location.href = "login.html";
+            }
         }
     });
 
